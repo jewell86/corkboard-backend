ddeaf24731615d44b4db4c0fee38bc556325ebf6
jewell86 <jewellsmailbox@gmail.com>
2018-09-19 22:52:33 -0700

    add pusher functionality to note item

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const { PORT = 5000, NODE_ENV = 'development' } = process.env
+let path = require('path');
+let Pusher = require('pusher');
+let bodyParser = require('body-parser');
+let pusher = new Pusher(require('./config.js'));
 
 if (NODE_ENV === 'development') {
   require('dotenv').load()
@@ -8,7 +12,14 @@ if (NODE_ENV === 'development') {
 }
 
 app.use(require('body-parser').json())
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('cors')())
+
+app.post('/update_text', function(req, res){
+  var payload = {text: req.body.text, deviceId: req.body.from}
+  pusher.trigger('collabo', 'text_update', payload)
+  res.json({success: 200})
+});
 
 app.use('/', require('./routes/users'))
 
---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  appId: '603570',
+  key: '68d00ab5cb679315179f',
+  secret: 'fa3f2cfd5a13ed9b3953',
+  cluster: 'us2',
+  encrypted: true
+};
