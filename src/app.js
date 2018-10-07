7a3ed22a528d9f2ec62a5ec217fb34aeb0931dab
jewell86 <jewellsmailbox@gmail.com>
2018-10-07 12:25:13 -0700

    add heroku configuration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { PORT = 5000, NODE_ENV = 'development' } = process.env
+const { PORT = 5000, NODE_ENV = 'production' } = process.env
 let path = require('path');
 let Pusher = require('pusher');
 let bodyParser = require('body-parser');
